a4d80af86cd3cc607f2c33182b88e89fad3fb704
incubated-geek-cc <xuemanxu.cc@gmail.com>
2021-12-23 04:22:58 +0800

    Updated code

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # node-api-stream-demo
 ### A demo app for streaming binary data to and from the Express NodeJS server.
+### To run it, just open a terminal (assuming you have npm and node installed), run the command `node server.js`
 
 ## Article at: 
--- server.js
+++ server.js
@@ -10,17 +10,59 @@ const path = require('path');
 // });
 
 app.get('/', (req, res) => {
-	var filepathToStream=path.join(__dirname, 'leaf.png');
-	var readStream = fs.createReadStream(filepathToStream);
+  var filepathToStream = path.join(__dirname, 'leaf.png');
+  var readStream = fs.createReadStream(filepathToStream);
 
-  	readStream
-  	.on('open', () => {
-		readStream.pipe(res);
-  	})
-  	.on('error', (err_msg) => {
-      	console.log(err_msg);
-      	res.end(err_msg);
-  	});
+  readStream
+  .on('open', () => {
+    readStream.pipe(res);
+  })
+  .on('error', (err_msg) => {
+  	console.log(err_msg);
+  	res.end(err_msg);
+  });
+});
+
+app.get('/leaf', (req, res) => {
+  var filepathToStream=path.join(__dirname, 'leaf.png');
+  var readStream = fs.createReadStream(filepathToStream);
+
+  readStream
+  .on('data', (chunk) => {
+    res.write(chunk);
+  })
+  .on('error', (err_msg) => {
+    console.log(err_msg);
+    res.end(err_msg);
+  })
+  .on('end', () => {
+      res.end();
+  });
+});
+
+
+app.get('/download_leaf', (req, res) => {
+  var allChunks=[];
+
+  var filepathToStream=path.join(__dirname, 'leaf.png');
+  var readStream = fs.createReadStream(filepathToStream);
+
+  readStream
+  .on('data', (chunk) => {
+    allChunks=allChunks.concat(chunk);
+  })
+  .on('error', (err_msg) => {
+    console.log(err_msg);
+    res.end(err_msg);
+  })
+  .on('end', () => {
+      var b64URI = 'data:image/png;base64,'+Buffer.from(allChunks[0]).toString('base64');
+      var downloadLink="<a href='"+b64URI+"' download='leaf.png' target='_blank'>Download leaf.png</a>";
+
+      res.send(downloadLink);
+
+      console.log('end');
+  });
 });
 
 app.listen(port, () => {
